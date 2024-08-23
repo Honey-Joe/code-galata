1922765602483e71ef8828998a4dd673ed030217
Honey-Joe <honeyjoe943@gmail.com>
2024-08-24 02:37:50 +0530

    sad

--- src/components/Contactroute/Contactroute.jsx
+++ src/components/Contactroute/Contactroute.jsx
@@ -1,10 +1,12 @@
 import React from 'react'
 import Body from './Body/Body'
+import Navbar from '../NavBar/Navbar'
 import Footer from '../Footer/Footer'
 
 const Contactroute = () => {
   return (
     <div>
+        <Navbar></Navbar>
       <Body></Body>
       <Footer></Footer>
     </div>
