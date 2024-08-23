96e4fcd8af7a5bd14307c0a1526f3aa74c8d3c3e
Honey-Joe <honeyjoe943@gmail.com>
2024-08-23 22:53:14 +0530

    code galata start

--- src/main.jsx
+++ src/main.jsx
@@ -1,10 +1,17 @@
-import { StrictMode } from 'react'
-import { createRoot } from 'react-dom/client'
-import App from './App.jsx'
-import './index.css'
+import ReactDOM from "react-dom/client"
+import Header from "./components/Header/Header";
+import Body from "./components/Body/Body";
+import Footer from "./components/Footer/Footer";
 
-createRoot(document.getElementById('root')).render(
-  <StrictMode>
-    <App />
-  </StrictMode>,
-)
+const Applayout  = ()=>{
+  return(
+    <>
+    <Header></Header>
+    <Body></Body>
+    <Footer></Footer>
+    </>
+  )
+}
+
+const root = ReactDOM.createRoot(document.getElementById("root"));
+root.render(<Applayout></Applayout>)
