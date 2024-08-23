0976b43688febff86fe13526b3d7da30f15b725f
Honey-Joe <honeyjoe943@gmail.com>
2024-08-24 03:11:44 +0530

    dsf

--- src/components/Aboutroute/Aboutroute.jsx
+++ src/components/Aboutroute/Aboutroute.jsx
@@ -1,11 +1,9 @@
 import React from 'react'
 import Body from './Body/Body'
-import Navbar from '../NavBar/Navbar'
 import Footer from '../Footer/Footer'
 const Aboutroute = () => {
   return (
     <>
-    <Navbar></Navbar>
     <Body></Body>
     <Footer></Footer>
     </>
