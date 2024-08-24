204c2e5133386d312293dd02c1ee8908761302db
Honey-Joe <honeyjoe943@gmail.com>
2024-08-25 00:16:38 +0530

    dfgdfg

--- src/components/Homecard/Homebody.jsx
+++ src/components/Homecard/Homebody.jsx
@@ -57,12 +57,12 @@ const Homebody = () => {
     <div className="2xl:container py-8">
       <div className="w-[90%] mx-auto grid grid-cols-1">
         <div className="">
-          <h1 className="text-[18px] text-blue-600 text-center">What we Provides?</h1>
-          <h2 className="text-[42px] font-semibold text-center">
+          <h1 className="text-[18px] text-blue-600 text-center py-5">What we Provides?</h1>
+          <h2 className="text-[42px] font-semibold text-center py-5">
             Provide Interective IT Solution Business Services
           </h2>
-          <div className="my-5">
-          <div className="grid grid-cols-1 sm:grid-cols-1 md:grid-cols-2 lg:grid-cols-4 gap-10 justify-center">
+          <div className="my-5 lg:block flex justify-center flex-wrap">
+          <div className="grid grid-cols-1 sm:grid-cols-1 md:grid-cols-2 lg:grid-cols-3 xl:grid-cols-4 gap-10 justify-center">
             {homes.map((home, index) => (
               <Homecard
                 key={index}
