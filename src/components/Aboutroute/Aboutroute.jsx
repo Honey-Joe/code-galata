fca09e7d8d352e74637a2822f0b5a731e4a6a741
Honey-Joe <honeyjoe943@gmail.com>
2024-08-24 22:56:00 +0530

    asdsad

--- src/components/Aboutroute/Aboutroute.jsx
+++ src/components/Aboutroute/Aboutroute.jsx
@@ -1,11 +1,9 @@
 import React from 'react'
 import Body from './Body/Body'
 import Footer from '../Footer/Footer'
-import Navbar from '../NavBar/Navbar'
 const Aboutroute = () => {
   return (
     <>
-    <Navbar></Navbar>
     <Body></Body>
     <Footer></Footer>
     </>
--- src/components/Contactroute/Contactroute.jsx
+++ src/components/Contactroute/Contactroute.jsx
@@ -1,12 +1,10 @@
 import React from 'react'
 import Body from './Body/Body'
 import Footer from '../Footer/Footer'
-import Navbar from '../Navbar/Navbar'
 
 const Contactroute = () => {
   return (
     <div>
-      <Navbar></Navbar>
       <Body></Body>
       <Footer></Footer>
     </div>
