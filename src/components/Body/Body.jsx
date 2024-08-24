994e07103b487e54f6728b2d27dc94bcd485a426
Honey-Joe <honeyjoe943@gmail.com>
2024-08-25 00:02:14 +0530

    dsfsdf

--- src/components/Body/Body.jsx
+++ src/components/Body/Body.jsx
@@ -4,11 +4,13 @@ import Topcourse from '../Topcourse/Topcourse'
 import Course from '../Course/Course'
 import NewsLetter from '../NewsLetter/NewsLetter'
 import HowWeWork from '../HowWeWork/HowWeWork'
+import Homebody from '../Homecard/Homebody'
 
 const Body = () => {
   return (
     <>
       {/* <Course></Course> */}
+      <Homebody></Homebody>
       <Topcourse></Topcourse>
        <HowWeWork/>
        <ReviewSection></ReviewSection>
