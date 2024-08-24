bee8271e3973784d06d1f29b730c69c09ca78d14
SudharsanMaruthaiyan <sudharsan6078@gmail.com>
2024-08-24 12:51:05 +0530

    hghjkjkhkljhklj

--- src/components/Contactroute/ContactForm/ContactForm.jsx
+++ src/components/Contactroute/ContactForm/ContactForm.jsx
@@ -13,25 +13,27 @@ const ContactForm = () => {
                     <h2 className="  text-center font-[LexendDeca] text-[14px] font-bold text-[#2F57EF] bg-[#E4E9FD] py-2 px-6  rounded-full w-fit">EDUCATION FOR EVERYONE</h2>
                     <h1 className=" md:text-[28px] lg:text-[34px] font-bold font-[LexendDeca] pt-5 leading-tight">Get a Free Course You Can Contact With Me</h1>
                     <div className=" pt-10">
-                        <div className=" relative">
-                            <input id="name" type="text" className=" border-b-2 py-1 focus:outline-none focus:border-[#2F57EF] focus:border-b-3 transition-colors peer w-full" />
-                            <label className=" absolute left-0 top-1 cursor-text focus:text-xs peer-focus:text-xs font-[LexendDeca] text-[18px] text-[#6b7385] peer-focus:-translate-y-6 transition -translate-y-2 peer-focus:text-[#2F57EF]" htmlFor="name">Name</label>
-                        </div>
-                        <div className=" relative my-12">
-                            <input id="email" type="text" className=" border-b-2 py-1 focus:outline-none focus:border-[#2F57EF] focus:border-b-3 transition-colors peer w-full" />
-                            <label className=" absolute left-0 top-1 cursor-text focus:text-xs peer-focus:text-xs font-[LexendDeca] text-[18px] text-[#6b7385] peer-focus:-translate-y-6 transition -translate-y-2 peer-focus:text-[#2F57EF]" htmlFor="email">Email</label>
-                        </div>
-                        <div className=" relative my-12">
-                            <input id="yoursubject" type="text" className=" border-b-2 py-1 focus:outline-none focus:border-[#2F57EF] focus:border-b-3 transition-colors peer w-full" />
-                            <label className=" absolute left-0 top-1 cursor-text focus:text-xs peer-focus:text-xs font-[LexendDeca] text-[18px] text-[#6b7385] peer-focus:-translate-y-6 transition -translate-y-2 peer-focus:text-[#2F57EF]" htmlFor="yoursubject">Your Subject</label>
-                        </div>
-                        <div className=" relative mb-10">
-                            <textarea className=" border-b-2 py-1 focus:outline-none focus:border-[#2F57EF] focus:border-b-3 transition-colors peer w-full" rows="4" name="message" id="message"></textarea>
-                            <label className=" absolute left-0 top-1 cursor-text focus:text-xs peer-focus:text-xs font-[LexendDeca] text-[18px] text-[#6b7385] peer-focus:-translate-y-6 transition -translate-y-2 peer-focus:text-[#2F57EF]" htmlFor="messages">Message</label>
-                        </div>
-                        <div className="">
-                            <Button/>
-                        </div>
+                        <form action="get">
+                            <div className=" relative">
+                                <input id="name" type="text" className=" border-b-2 py-1 focus:outline-none focus:border-[#2F57EF] focus:border-b-3 transition-colors peer w-full" />
+                                <label className=" absolute left-0 top-1 cursor-text focus:text-xs peer-focus:text-xs font-[LexendDeca] text-[18px] text-[#6b7385] peer-focus:-translate-y-6 transition -translate-y-2 peer-focus:text-[#2F57EF]" htmlFor="name">Name</label>
+                            </div>
+                            <div className=" relative my-12">
+                                <input id="email" type="text" className=" border-b-2 py-1 focus:outline-none focus:border-[#2F57EF] focus:border-b-3 transition-colors peer w-full" />
+                                <label className=" absolute left-0 top-1 cursor-text focus:text-xs peer-focus:text-xs font-[LexendDeca] text-[18px] text-[#6b7385] peer-focus:-translate-y-6 transition -translate-y-2 peer-focus:text-[#2F57EF]" htmlFor="email">Email</label>
+                            </div>
+                            <div className=" relative my-12">
+                                <input id="yoursubject" type="text" className=" border-b-2 py-1 focus:outline-none focus:border-[#2F57EF] focus:border-b-3 transition-colors peer w-full" />
+                                <label className=" absolute left-0 top-1 cursor-text focus:text-xs peer-focus:text-xs font-[LexendDeca] text-[18px] text-[#6b7385] peer-focus:-translate-y-6 transition -translate-y-2 peer-focus:text-[#2F57EF]" htmlFor="yoursubject">Your Subject</label>
+                            </div>
+                            <div className=" relative mb-10">
+                                <textarea className=" border-b-2 py-1 focus:outline-none focus:border-[#2F57EF] focus:border-b-3 transition-colors peer w-full" rows="4" name="message" id="message"></textarea>
+                                <label className=" absolute left-0 top-1 cursor-text focus:text-xs peer-focus:text-xs font-[LexendDeca] text-[18px] text-[#6b7385] peer-focus:-translate-y-6 transition -translate-y-2 peer-focus:text-[#2F57EF]" htmlFor="messages">Message</label>
+                            </div>
+                            <div className="">
+                                <Button/>
+                            </div>
+                        </form>
                     </div>
                 </div>
             </div>
--- src/components/Navbar/Navbar.jsx
+++ src/components/Navbar/Navbar.jsx
@@ -1,47 +1,46 @@
 import { Menu } from "lucide-react";
 import { navlogo } from "../../assets/image";
 import { Link } from "react-router-dom";
+import { useState } from "react";
 const Navbar = () => {
+  const [toggle, setToggle] = useState(false)
   return (
     <>
-      <div className="2xl:container mx-auto bg-white">
-        <div className="w-[90%]  grid grid-cols-2 mx-auto my-5 items-center justify-between">
+      <div className="3xl:container mx-auto bg-white shadow-sm fixed top-0 w-full">
+        <div className=" w-[95%] grid grid-cols-2 lg:grid-cols-3 mx-auto my-5 items-center justify-between sticky top-0">
           <div className="">
             <img src={navlogo} alt="logo" width={100} />
           </div>
-          <div className="flex items-center justify-end">
-            <div className="lg:block hidden" id="show">
-              <div className=" lg:flex gap-20 absolute  left-0 top-20 lg:static bg-white w-[100%] z-50">
+          <div className="flex items-center justify-end lg:col-span-2 ">
+            <div className={`lg:block ${toggle?'block ' :' hidden'}`}>
+              <div className=" lg:flex gap-20 absolute left-0 top-20 lg:static bg-white w-[100%] lg:mt-0 -mt-6 z-50 ">
                 <div>
-                  <ul className=" lg:flex gap-4 items-center justify-center ">
-                    <li className=" font-normal text-lg font-[LexendDeca] p-2 text-center lg:p-0 hover:text-[#1176F0]">
+                  <ul className=" lg:flex lg:gap-9 space-y-3 lg:space-y-0 items-center justify-center ">
+                    <li className=" text-[15px] font-bold font-[poppins] p-2 text-center lg:p-0 hover:text-[#1176F0]">
                       <Link to={"/"}>Home</Link>
                     </li>
-                    <li className=" font-normal text-lg font-[LexendDeca] p-2 text-center lg:p-0  hover:text-[#1176F0]">
+                    <li className="text-[15px] font-bold font-[poppins] p-2 text-center lg:p-0  hover:text-[#1176F0]">
                       <Link to={"/course"}>Course</Link>
                     </li>
-                    <li className=" font-normal text-lg font-[LexendDeca] p-2 text-center lg:p-0 hover:text-[#1176F0]">
+                    <li className="text-[15px] font-bold font-[poppins] p-2 text-center lg:p-0 hover:text-[#1176F0]">
                       <Link to={"/about"}>About us</Link>
                     </li>
-                    <li className=" font-normal text-lg font-[LexendDeca] p-2 text-center lg:p-0 hover:text-[#1176F0]">
+                    <li className="text-[15px] font-bold font-[poppins] p-2 text-center lg:p-0 hover:text-[#1176F0]">
                       <Link to={"/contact"}>Contact</Link>
                     </li>
-                    <li className=" font-normal text-lg font-[LexendDeca] p-2 text-center lg:p-0 hover:text-[#1176F0]">
+                    <li className="text-[15px] font-bold font-[poppins] p-2 text-center lg:p-0 hover:text-[#1176F0]">
                       <p>Blog</p>
                     </li>
-                    <li className=" font-normal text-lg font-[LexendDeca] p-2 text-center lg:p-0  px-3 py-2 rounded-lg flex items-center  text-white justify-center">
-                      <button className="bg-blue-500 px-3 py-1 rounded-lg text-white ">Log in </button>
+                    <li className="text-[15px] font-bold font-[poppins] p-2 text-center lg:p-0  px-3 py-2 rounded-lg flex items-center  text-white justify-center">
+                      <button className="bg-blue-500 px-8 py-2 hover:bg-white border border-[#1176F0] hover:text-[#1176F0] rounded-full font-[poppins] font-bold text-white ">Log in </button>
                     </li>
                   </ul>
                 </div>
               </div>
             </div>
-            <Menu
-              className="lg:hidden"
-              onClick={() => {
-                const nav = document.getElementById("show");
-                nav.classList.toggle("hidden");
-              }}
+            <Menu className={`block lg:hidden`} onClick={()=>{
+              setToggle(!toggle)
+            }} 
             ></Menu>
           </div>
         </div>
@@ -49,5 +48,4 @@ const Navbar = () => {
     </>
   );
 };
-
 export default Navbar;
