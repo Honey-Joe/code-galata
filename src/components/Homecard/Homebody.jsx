3180a58173b261e99d6b05b9e7ecbae2cfe42b2b
Honey-Joe <honeyjoe943@gmail.com>
2024-08-25 00:09:49 +0530

    ddfs

--- src/components/Homecard/Homebody.jsx
+++ src/components/Homecard/Homebody.jsx
@@ -38,18 +38,31 @@ const homes = [
     content:
       "Monotonectally synergize grants to business visualize strategic infomediaries",
   },
+  {
+    image: "https://ik.imagekit.io/sri05/service-1.png?updatedAt=1721654804318",
+    name: "Graphic Design",
+    content:
+      "Monotonectally synergize grants to business visualize strategic infomediaries",
+  },
+  {
+    image: "https://ik.imagekit.io/sri05/service-1.png?updatedAt=1721654804318",
+    name: "Graphic Design",
+    content:
+      "Monotonectally synergize grants to business visualize strategic infomediaries",
+  },
 ];
 
 const Homebody = () => {
   return (
-    <div className="2xl:container mx-auto py-8">
-      <div className="w-[90%] mx-auto">
-        <div className="flex flex-col justify-center items-center gap-7">
-          <h1 className="text-[18px] text-blue-600">What we Provides?</h1>
-          <h2 className="text-[42px] font-semibold">
+    <div className="2xl:container py-8">
+      <div className="w-[90%] mx-auto grid grid-cols-1">
+        <div className="">
+          <h1 className="text-[18px] text-blue-600 text-center">What we Provides?</h1>
+          <h2 className="text-[42px] font-semibold text-center">
             Provide Interective IT Solution Business Services
           </h2>
-          <div className="grid grid-cols-1 sm:grid-cols-1 md:grid-cols-2 lg:grid-cols-3 gap-6">
+          <div className="my-5">
+          <div className="grid grid-cols-1 sm:grid-cols-1 md:grid-cols-2 lg:grid-cols-4 gap-10 justify-center">
             {homes.map((home, index) => (
               <Homecard
                 key={index}
@@ -58,6 +71,8 @@ const Homebody = () => {
               />
             ))}
           </div>
+          </div>
+          
         </div>
       </div>
     </div>
