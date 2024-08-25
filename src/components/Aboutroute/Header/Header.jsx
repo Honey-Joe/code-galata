56bf09b61dbf9176351e8cfb083eae16679b5a59
SudharsanMaruthaiyan <sudharsan6078@gmail.com>
2024-08-25 13:41:09 +0530

    done done

--- src/components/Aboutroute/Header/Header.jsx
+++ src/components/Aboutroute/Header/Header.jsx
@@ -5,11 +5,11 @@ const Header = () => {
     <>
      <div className="2xl:container mx-auto mt-[110px]">
         <div className="w-[100%] grid grid-cols-1 mx-auto">
-        <img src={abouth440} alt=""  className="hidden md:block xl:hidden"/>
+          <img src={abouth440} alt=""  className="hidden md:block xl:hidden"/>
           <img src={abouth} alt="" className="hidden xl:block"/>
           <img src={abouth720} alt="" className=" md:hidden xl:hidden"/>
         </div>
-        </div> 
+      </div> 
 
     </>
   )
